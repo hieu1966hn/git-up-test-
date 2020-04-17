c3e1f7e6a90f591ec4f999069043d024e94810e5
NGUYEN TRUNG HIEU <hieu1966hn@gmail.com>
2020-04-17 15:30:03 +0700

    first commit

---
+++ biến trong js/bien.js
@@ -0,0 +1,6 @@
+function add_comment(){
+    let comment = "xin chao moi nguoi nhe";
+    alert(comment);
+}
+let n;
+add_comment(n);
---
+++ biến trong js/bien_cuc_bo.js
@@ -0,0 +1,16 @@
+// let comment = "day la noi dung comment toan cuc";
+// function add_comment(){
+//     let comment = "day la noi dung comment cuc bo ";
+//     alert(comment);
+// }
+// add_comment();
+// alert(comment);
+
+///////////////
+let comment = "nd bien toan cuc ";
+function add_comment(){
+   let comment = "nd bien da bi thay doi";
+    alert(comment);
+}
+add_comment();
+alert(comment);
